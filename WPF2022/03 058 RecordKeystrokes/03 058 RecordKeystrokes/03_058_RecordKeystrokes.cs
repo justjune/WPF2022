4d27ec90b568ea0dc5d2e70943042d5acdc57877
romanov_maksim <88290162+Makson4ik001@users.noreply.github.com>
2022-06-07 00:37:56 +0500

    Trembach Aleksei 03 058

---
+++ WPF2022/03 058 RecordKeystrokes/03 058 RecordKeystrokes/03_058_RecordKeystrokes.cs
@@ -0,0 +1,37 @@
+﻿using System; 
+using System.Windows; 
+using System.Windows.Input; 
+using System.Windows.Media; 
+
+namespace Petzold.RecordKeystrokes 
+{     
+    public class RecordKeystrokes : Window     
+    {         
+        [STAThread]         
+        public static void Main()         
+        {             
+            Application app = new Application();             
+            app.Run(new RecordKeystrokes());         
+        }         
+        public RecordKeystrokes()         
+        {             
+            Title = "Record Keystrokes";             
+            Content = "ssfsfs";         
+        }         
+        protected override void OnTextInput (TextCompositionEventArgs args)         
+        {             
+            base.OnTextInput(args);             
+            string str = Content as string;             
+            if (args.Text == "\b")             
+            {             
+                if (str.Length > 0)                 
+                    str = str.Substring(0, str.Length  - 1);             
+            }             
+            else             
+            {                 
+                str += args.Text;             
+            }             
+            Content = str;         
+        }     
+    }  
+}
