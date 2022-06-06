94aee06dec806fcf9e8d02ee7da510f4deab9ad5
romanov_maksim <88290162+Makson4ik001@users.noreply.github.com>
2022-06-06 16:17:44 +0500

    Romanov Maksim add 05 097 10 200 13 271 19 494

---
+++ WPF2022/19 494 UseCustomClass/19 494 UseCustomClass/19_494_UseCustomClass.xaml.cs
@@ -0,0 +1,31 @@
+﻿using System; 
+using System.Windows; 
+using System.Windows.Controls;
+using Petzold.SelectColorFromGrid;
+using System.Windows.Input; 
+using System.Windows.Media;
+using Microsoft.Win32;
+using System.IO;
+using System.Windows.Markup;
+using System.Xml;
+namespace Petzold.UseCustomClass 
+{     
+    public partial class UseCustomClass : Window     
+    {         
+        [STAThread]         
+        public static void Main()         
+        {             
+            Application app = new Application();             
+            app.Run(new UseCustomClass());         
+        }         
+        public UseCustomClass()         
+        {             
+            InitializeComponent();         
+        }         
+        void ColorGridBoxOnSelectionChanged(object  sender,SelectionChangedEventArgs args)         
+        {             
+            ColorGridBox clrbox = args.Source as  ColorGridBox;             
+            Background = (Brush) clrbox.SelectedValue;         
+        }     
+    } 
+}
