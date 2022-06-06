858306b4a3aa11c8c5832d3a0b5006cd9523bc88
romanov_maksim <88290162+Makson4ik001@users.noreply.github.com>
2022-06-07 00:11:10 +0500

    Trembach Aleksei 14 314

---
+++ WPF2022/14 314 ControlXCV/14 314 ControlXCV/14_314_ControlXCV.cs
@@ -0,0 +1,106 @@
+﻿using System; 
+using System.Windows; 
+using System.Windows.Controls; 
+using System.Windows.Input; 
+using System.Windows.Media; 
+namespace Petzold.CutCopyAndPaste 
+{     
+    public class CutCopyAndPaste : Window     
+    {         
+        TextBlock text;         
+        protected MenuItem itemCut, itemCopy,  itemPaste, itemDelete;         
+       
+        public CutCopyAndPaste()         
+        {             
+            Title = "Cut, Copy, and Paste";             
+            // Create DockPanel.
+            DockPanel dock = new DockPanel();             
+            Content = dock;             
+            // Create Menu docked at top.
+            Menu menu = new Menu();             
+            dock.Children.Add(menu);             
+            DockPanel.SetDock(menu, Dock.Top);             
+            // Create TextBlock filling the rest.
+            text = new TextBlock();             
+            text.Text = "Sample clipboard text";             
+            text.HorizontalAlignment =  HorizontalAlignment.Center;             
+            text.VerticalAlignment =  VerticalAlignment.Center;             
+            text.FontSize = 32;             
+            text.TextWrapping = TextWrapping.Wrap;             
+            dock.Children.Add(text);             
+            // Create Edit menu.
+            MenuItem itemEdit = new MenuItem();             
+            itemEdit.Header = "_Edit";             
+            itemEdit.SubmenuOpened += EditOnOpened;             
+            menu.Items.Add(itemEdit);             
+            // Create items on Edit menu.
+            itemCut = new MenuItem();             
+            itemCut.Header = "Cu_t";             
+            itemCut.Click += CutOnClick;             
+                    
+        }         
+        void EditOnOpened(object sender,  RoutedEventArgs args)         
+        {             
+            itemCut.IsEnabled = itemCopy.IsEnabled = itemDelete.IsEnabled = text.Text !=  null && text.Text.Length > 0;             
+            itemPaste.IsEnabled = Clipboard .ContainsText();         
+        }         
+        protected void CutOnClick(object sender,  RoutedEventArgs args)         
+        {             
+            CopyOnClick(sender, args);             
+            DeleteOnClick(sender, args);         
+        }         
+        protected void CopyOnClick(object sender,  RoutedEventArgs args)         
+        {             
+            if (text.Text != null && text.Text. Length > 0)                 
+                Clipboard.SetText(text.Text);         
+        }         
+        protected void PasteOnClick(object sender,  RoutedEventArgs args)         
+        {             
+            if (Clipboard.ContainsText())                 
+                text.Text = Clipboard.GetText();         
+        }         
+        protected void DeleteOnClick(object sender , RoutedEventArgs args)         
+        {             
+            text.Text = null;         
+        }     
+    } 
+}
+namespace Petzold.ControlXCV 
+{     
+    public class ControlXCV : Petzold .CutCopyAndPaste.CutCopyAndPaste     
+    {         
+        KeyGesture gestCut = new KeyGesture(Key.X,  ModifierKeys.Control);         
+        KeyGesture gestCopy = new KeyGesture(Key.C , ModifierKeys.Control);         
+        KeyGesture gestPaste = new KeyGesture(Key .V, ModifierKeys.Control);         
+        KeyGesture gestDelete = new KeyGesture(Key .Delete);         
+        [STAThread]         
+        public new static void Main()         
+        {             
+            Application app = new Application();             
+            app.Run(new ControlXCV());         
+        }         
+        public ControlXCV()         
+        {             
+            Title = "Control X, C, and V";             
+            itemCut.InputGestureText = "Ctrl+X";             
+            itemCopy.InputGestureText = "Ctrl+C";             
+            itemPaste.InputGestureText = "Ctrl+V";             
+            itemDelete.InputGestureText = "Delete";         
+        }         
+        protected override void OnPreviewKeyDown (KeyEventArgs args)         
+        {             
+            base.OnKeyDown(args);             
+            args.Handled = true;             
+            if (gestCut.Matches(null, args))                 
+                CutOnClick(this, args);             
+            else if (gestCopy.Matches(null, args))                 
+                CopyOnClick(this, args);             
+            else if (gestPaste.Matches(null, args))                 
+                PasteOnClick(this, args);             
+            else if (gestDelete.Matches(null, args))                 
+                DeleteOnClick(this, args);             
+            else                 
+                args.Handled = false;         
+        }     
+    } 
+}
