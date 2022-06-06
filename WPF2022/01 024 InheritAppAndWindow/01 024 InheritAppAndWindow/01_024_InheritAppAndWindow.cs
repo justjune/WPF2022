4b9fd3927b2b1bd0b62e007f134930d9550b8ba0
romanov_maksim <88290162+Makson4ik001@users.noreply.github.com>
2022-06-06 16:44:23 +0500

    Trembach Aleksei 01 024

---
+++ WPF2022/01 024 InheritAppAndWindow/01 024 InheritAppAndWindow/01_024_InheritAppAndWindow.cs
@@ -0,0 +1,44 @@
+﻿using System; 
+using System.Windows; 
+using System.Windows.Input; 
+
+namespace Petzold.InheritAppAndWindow 
+{     
+    public class MyWindow : Window     
+    {         
+        public MyWindow()         
+        {             
+            Title = "Inherit App & Window";         
+        }         
+        protected override void OnMouseDown (MouseButtonEventArgs args)         
+        {             
+            base.OnMouseDown(args);             
+            string strMessage = string.Format("Window clicked with  {0} button at point ({1})",args.ChangedButton,args.GetPosition(this));             
+            MessageBox.Show(strMessage, Title);         
+        }     
+    } 
+}
+namespace Petzold.InheritAppAndWindow 
+{     
+    class MyApplication : Application     
+    {         
+        protected override void OnStartup (StartupEventArgs args)         
+        {             
+            base.OnStartup(args);             
+            MyWindow win = new MyWindow();             
+            win.Show();         
+        }     
+    } 
+}
+namespace Petzold.InheritAppAndWindow 
+{     
+    class InheritAppAndWindow     
+    {         
+        [STAThread]         
+        public static void Main()         
+        {             
+            MyApplication app = new MyApplication();             
+            app.Run();         
+        }     
+    } 
+}
