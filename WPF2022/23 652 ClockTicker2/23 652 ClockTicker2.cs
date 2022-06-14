41515911589fe824e3dbdc4486e64cddc7d9af17
SmallRiot <76480637+SmallRiot@users.noreply.github.com>
2022-06-14 22:17:25 +0500

    FedinAdievCpicin

---
+++ WPF2022/23 652 ClockTicker2/23 652 ClockTicker2.cs
@@ -0,0 +1,42 @@
+﻿using System;
+using System.ComponentModel;
+using System.Windows;
+using System.Windows.Threading;
+namespace Petzold.FormattedDigita1Clock
+{ /*Класс ClockTicker2 является частью нового проекта FormattedDigitalClock, 
+который не только использует альтернативный способ оповещения об изменениях
+свойства DateTime, по и предоставляет автору кода XAML большую гибкость
+при форматировании объекта DateTime.
+Формат даты и времени в программе DigitalClock определялся вызовом 
+ToString для объекта DateTime. Однако в классе DateTime определяется 
+перегруженная версия метода ToString, которая получает дополнительный аргумент с 
+форматной строкой и может выводить дату и время в разных форматах. Например,
+в формате «Т>> выводит время с секундами, а в формате «t» -
+время без секунд.*/
+public class ClockTicker2 : INotifyPropertyChanged
+{ 
+    // Событие, необходимое для интерфейса INotifyPropertyChanged
+    public event PropertyChangedEventHandler PropertyChanged;
+    // Открытое свойство
+    public DateTime DateTime
+    {
+        get { return DateTime.Now; }
+    }
+    // Установка таймера в конструкторе
+    public ClockTicker2()
+    {
+        DispatcherTimer timer =
+        new DispatcherTimer();
+        timer.Tick += TimerOnTick;
+        timer.Interval =
+        TimeSpan.FromSeconds(1);
+        timer.Start();
+    }
+    // Обработчик собьпил таймера инициирует событие PropertyChanged
+    void TimerOnTick(object sender, EventArgs args)
+    {
+            if (PropertyChanged != null)
+                PropertyChanged(this,new PropertyChangedEventArgs("DateTime"));
+}
+}
+}
---
+++ WPF2022/LoadXamlResource 19-482/Project2/LoadXamlResource 19-482.cs
@@ -0,0 +1,46 @@
+﻿namespace Petzold.LoadXamlResource
+{
+    using System;
+    using System.IO;
+    using System.Windows;
+    using System.Windows.Controls;
+    using System.Windows.Markup;
+    using System.Xml;
+
+    public class LoadXamlResource : Window
+    {
+        #region Constructors
+
+        public LoadXamlResource()
+        {
+            Title = "Load Xaml Resource";
+
+            StreamReader sr = new StreamReader("LoadXamlResource.xml");
+            XmlTextReader xmlreader = new XmlTextReader(sr);
+            FrameworkElement el = XamlReader.Load(xmlreader) as FrameworkElement;
+            Content = el;
+
+            Button btn = el.FindName("MyButton") as Button;
+            if (btn != null)
+                btn.Click += ButtonOnClick;
+        }
+
+        #endregion Constructors
+
+        #region Methods
+
+        [STAThread]
+        public static void Main()
+        {
+            Application app = new Application();
+            app.Run(new LoadXamlResource());
+        }
+
+        void ButtonOnClick(object sender, RoutedEventArgs args)
+        {
+            MessageBox.Show("The button labeled '" + (args.Source as Button).Content + "' has been clicked");
+        }
+
+        #endregion Methods
+    }
+}
