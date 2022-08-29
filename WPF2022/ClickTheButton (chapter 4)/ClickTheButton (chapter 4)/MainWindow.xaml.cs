bf05af2a35e787a16a39be616299647133b7363a
ZaharovLeonid <90400619+ZaharovLeonid@users.noreply.github.com>
2022-08-29 11:45:59 +0300

    Add files via upload

---
+++ WPF2022/ClickTheButton (chapter 4)/ClickTheButton (chapter 4)/MainWindow.xaml.cs
@@ -0,0 +1,39 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Windows;
+using System.Windows.Controls;
+using System.Windows.Data;
+using System.Windows.Documents;
+using System.Windows.Input;
+using System.Windows.Media;
+using System.Windows.Media.Imaging;
+using System.Windows.Navigation;
+using System.Windows.Shapes;
+
+namespace ClickTheButton__chapter_4_
+{
+    /// <summary>
+    /// Interaction logic for MainWindow.xaml
+    /// </summary>
+    public partial class MainWindow : Window
+    {
+        public MainWindow()
+        {
+            Window win = new Window(); 
+            win.Title = "Click the Button";
+            Button btn = new Button();
+            btn.Content = "_Click me, please!";
+            btn.Click += ButtonOnClick; 
+            Content = btn;
+            win.Show();
+            InitializeComponent();
+        }
+        void ButtonOnClick(object sender, RoutedEventArgs args)
+        {
+            MessageBox.Show("The button has been  clicked and all is well.", Title);
+        }
+    }
+}
